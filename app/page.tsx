6d27ca2a88906905180b00cb317795c8c812bfb8
creativeloc <creativeloc@yahoo.com>
2026-05-16 02:29:09 -0400

    create button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+import ExploreBtn from "@/components/ExploreBtn";
+
 const Home = () => {
   return (
     <section>
       <h1 className="text-center">The Hub for Every Dev  <br/>Event you should miss</h1>
       <p className="text-center mt-5">Hackathons, Meetups, and Conferences, all in one place</p>
+
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {[1,2,3,4,5].map((event) => (
+              <li key={event}>Event {event}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
